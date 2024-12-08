ff147bb62ccfaa8b2e857cad7edd44ba1f782401
AYUSH THAKUR <92247357+ayushhthakurr@users.noreply.github.com>
2024-12-08 15:47:20 +0530

    prompt

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,5 +1,6 @@
 require("dotenv").config();
 import Groq from "groq-sdk";
+import { getSystemPrompt } from "./prompt";
 
 const groq = new Groq();
 
@@ -7,10 +8,9 @@ async function main() {
   const completion = await groq.chat.completions.create({
     model: "llama3-8b-8192",
     max_tokens:1024,
-    temperature:0.5,
     messages: [
         {
-          role: "user",
+          role: "system",
           content: "What is 2+2",
         },
       ],
