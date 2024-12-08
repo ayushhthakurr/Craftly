438cf066b272bf4dbd4712b4598d7e5d4e8af536
AYUSH THAKUR <92247357+ayushhthakurr@users.noreply.github.com>
2024-12-08 18:12:56 +0530

    prompt

--- be/dist/index.js
+++ be/dist/index.js
@@ -19,13 +19,13 @@ function main() {
     return __awaiter(this, void 0, void 0, function* () {
         const completion = yield groq.chat.completions.create({
             model: "llama3-8b-8192",
-            max_tokens: 1024,
-            temperature: 0.5,
+            max_tokens: 8000,
             messages: [
                 {
-                    role: "user",
-                    content: "What is 2+2",
-                },
+                    role: "system", content: "For all designs I ask you to make, have them be beautiful, not cookie cutter. Make webpages that are fully featured and worthy for production.\n\nBy default, this template supports JSX syntax with Tailwind CSS classes, React hooks, and Lucide React for icons. Do not install other packages for UI themes, icons, etc unless absolutely necessary or I request them.\n\nUse icons from lucide-react for logos.\n\nUse stock photos from unsplash where appropriate, only valid URLs you know exist. Do not download the images, only link to them in image tags.\n\n",
+                }, {
+                    role: 'user', content: "create a to do app"
+                }
             ],
         });
         console.log(completion.choices[0].message.content);
